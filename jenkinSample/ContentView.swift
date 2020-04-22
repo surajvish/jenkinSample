9b5caa93be0693e5cd2a5681c5eb1148c35f5baf
Suraj Vishwakarma <suraj.vish55@gmail.com>
2020-04-22 23:25:45 +0530

    Initial Commit

---
+++ jenkinSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jenkinSample
+//
+//  Created by Suraj Vishwakarma on 22/04/20.
+//  Copyright © 2020 Suraj Vishwakarma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
